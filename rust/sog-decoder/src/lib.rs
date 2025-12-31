cd29ccd7f92ae319da4440c79c691c9c4a2b9c53
drumath2237 <2237k.drumath@gmail.com>
2025-12-31 21:11:44 +0900

    :sparkles: draft impl for zip

--- rust/sog-decoder/src/lib.rs
+++ rust/sog-decoder/src/lib.rs
@@ -1,5 +1,45 @@
+use std::io::Cursor;
+
+use zip::{ZipArchive, result::ZipError};
+
+use std::io::Read;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
+}
+
+enum ArchivedSogFile {
+    MetaJson(String),
+    Image { filename: String, data: Vec<u8> },
+}
+
+fn extract_zip(zip_file_data: &[u8]) -> Result<Vec<ArchivedSogFile>, ZipError> {
+    let cursor = Cursor::new(zip_file_data);
+    let mut archive = ZipArchive::new(cursor)?;
+
+    let mut files = Vec::new();
+    for i in 0..archive.len() {
+        let mut zip_file = archive.by_index_raw(i)?;
+
+        let name = zip_file.name().to_owned();
+
+        let sog = if name == "meta.json" {
+            let mut buf = String::new();
+            zip_file.read_to_string(&mut buf)?;
+            ArchivedSogFile::MetaJson(buf)
+        } else {
+            let mut buf = Vec::new();
+            let _size = zip_file.read_to_end(&mut buf)?;
+            ArchivedSogFile::Image {
+                filename: name,
+                data: buf,
+            }
+        };
+
+        files.push(sog);
+    }
+
+    Ok(files)
 }
 
 #[cfg(test)]
