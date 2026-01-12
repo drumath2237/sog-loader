d651c4b1261b9a986f9b68f61105ac1c4d5f3c52
drumath2237 <2237k.drumath@gmail.com>
2026-01-13 03:12:51 +0900

    :sparkles: test with data
    :bug: splat count error

--- crates/playground/src/main.rs
+++ crates/playground/src/main.rs
@@ -1,12 +1,16 @@
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let sog_file = fs::read("./crates/sample_data/pizza.sog")?;
+    let sog_file = fs::read("./crates/sample_data/sushi.sog")?;
 
     let unzipped = sog_decoder::decode::unzip(&sog_file)?;
     let sog_data = sog_decoder::decode::parse_sog(unzipped)?;
 
-    println!("count: {}", sog_data.count);
+    println!("splat count:{}", sog_data.count);
+
+    let splat = sog_decoder::decode::decode_sog(&sog_data)?;
+
+    println!("count: {:?}", splat);
 
     Ok(())
 }
