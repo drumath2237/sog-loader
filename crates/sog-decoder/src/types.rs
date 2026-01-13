c93a070fb7622d4fad7f18860dd235b61f895b7a
drumath2237 <2237k.drumath@gmail.com>
2026-01-14 03:38:53 +0900

    :zap: fix quaternion order

--- crates/sog-decoder/src/types.rs
+++ crates/sog-decoder/src/types.rs
@@ -28,15 +28,15 @@ impl TryFrom<Vec<f32>> for Vector3 {
 
 #[derive(Debug, Clone, Default)]
 pub struct Quaternion {
-    pub w: f32,
     pub x: f32,
     pub y: f32,
     pub z: f32,
+    pub w: f32,
 }
 
 impl Quaternion {
-    pub fn new(w: f32, x: f32, y: f32, z: f32) -> Self {
-        Self { w, x, y, z }
+    pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
     }
 }
 
