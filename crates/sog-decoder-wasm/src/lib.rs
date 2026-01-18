0d92d8074f619c984795562bbfc236d81963be9f
drumath2237 <2237k.drumath@gmail.com>
2026-01-18 21:43:26 +0900

    :sparkles: change name

--- crates/sog-decoder-wasm/src/lib.rs
+++ crates/sog-decoder-wasm/src/lib.rs
@@ -1,17 +1,17 @@
 mod types;
 
-use crate::types::{JsSogDataV2, JsSplat};
+pub use crate::types::{JsSogDataV2, JsSplat};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(js_name = "unpackSog")]
-pub fn unpack_sog(buffer: &[u8]) -> Result<JsSogDataV2, JsError> {
+#[wasm_bindgen]
+pub fn unpack(buffer: &[u8]) -> Result<JsSogDataV2, JsError> {
     let sog = sog_decoder::unpack(buffer)?.into();
     Ok(sog)
 }
 
-#[wasm_bindgen(js_name = "decodeSplat")]
-pub fn decode_splat(js_sog_data: &JsSogDataV2) -> Result<JsSplat, JsError> {
-    let sog_data = js_sog_data.try_into()?;
-    let splat = sog_decoder::decode(&sog_data)?.into();
+#[wasm_bindgen]
+pub fn decode(js_sog_data: &JsSogDataV2) -> Result<JsSplat, JsError> {
+    let sog = js_sog_data.try_into()?;
+    let splat = sog_decoder::decode(&sog)?.into();
     Ok(splat)
 }
