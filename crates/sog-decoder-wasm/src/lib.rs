e6786139348c240960d5d259fef92e4cb3ddbcda
drumath2237 <2237k.drumath@gmail.com>
2026-01-20 22:19:51 +0900

    :zap: avoid clone huge vec

--- crates/sog-decoder-wasm/src/lib.rs
+++ crates/sog-decoder-wasm/src/lib.rs
@@ -10,7 +10,7 @@ pub fn unpack(buffer: &[u8]) -> Result<JsSogDataV2, JsError> {
 }
 
 #[wasm_bindgen]
-pub fn decode(js_sog_data: &JsSogDataV2) -> Result<JsSplat, JsError> {
+pub fn decode(js_sog_data: JsSogDataV2) -> Result<JsSplat, JsError> {
     let sog = js_sog_data.try_into()?;
     let splat = sog_decoder::decode(&sog)?.into();
     Ok(splat)
