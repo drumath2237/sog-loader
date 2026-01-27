f6b77887ca9b3c69e3355fb6fb79b0063982ffe1
drumath2237 <2237k.drumath@gmail.com>
2026-01-28 03:39:09 +0900

    :sparkles: try disposable

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -5,8 +5,8 @@ import sog_path from "../../../crates/sample_data/pizza.sog?url";
 
 async function main() {
   const sog_file = await fetch(sog_path).then((res) => res.arrayBuffer());
-  const sog = a(new Uint8Array(sog_file));
-  const splat = b(sog);
+  using sog = a(new Uint8Array(sog_file));
+  using splat = b(sog);
 
   console.log(splat);
 }
