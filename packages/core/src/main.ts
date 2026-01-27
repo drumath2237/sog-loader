65732911aaf613e4a65a24913c2dd68bc79a8686
drumath2237 <2237k.drumath@gmail.com>
2026-01-28 03:46:18 +0900

    :fire: fix

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -5,7 +5,7 @@ import sog_path from "../../../crates/sample_data/pizza.sog?url";
 
 async function main() {
   const sog_file = await fetch(sog_path).then((res) => res.arrayBuffer());
-  using sog = a(new Uint8Array(sog_file));
+  const sog = a(new Uint8Array(sog_file));
   using splat = b(sog);
 
   console.log(splat);
