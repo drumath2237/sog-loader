31081a68affd653648b31f9268a571519545ab5c
drumath2237 <2237k.drumath@gmail.com>
2026-01-28 03:22:23 +0900

    :package: vite plugin top level await

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,13 +1,15 @@
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [wasm()],
+  plugins: [wasm(), topLevelAwait()],
   build: {
     lib: {
       entry: "./lib/main.ts",
       name: "Counter",
       fileName: "counter",
+      formats: ["es", "system"],
     },
   },
 });
