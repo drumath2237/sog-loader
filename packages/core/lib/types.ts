e4ed46750f6f4c4e32d3acd06f51511f5f792d81
drumath2237 <2237k.drumath@gmail.com>
2026-02-23 00:05:59 +0900

    :sparkles: define a Splat type

---
+++ packages/core/lib/types.ts
@@ -0,0 +1,10 @@
+export type Splat = {
+  antialias: boolean;
+  count: number;
+  position: Float32Array;
+  rotation: Float32Array;
+  scale: Float32Array;
+  sh0: Float32Array;
+  shN?: Float32Array;
+  sh_degree: number;
+};
