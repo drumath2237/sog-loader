c20c907acb1fa59269a52c2c8cc5a92ae6ab74ac
drumath2237 <2237k.drumath@gmail.com>
2026-03-17 09:18:15 +0900

    :zap: improve performance when using raw splat

--- packages/babylonjs/lib/api.ts
+++ packages/babylonjs/lib/api.ts
@@ -4,7 +4,12 @@ import {
   type Scene,
   Vector3,
 } from "@babylonjs/core";
-import { decode, type RawSplat, type Splat, unpackRaw } from "@sog-loader/core";
+import {
+  decodeRaw,
+  type RawSplat,
+  type Splat,
+  unpackRaw,
+} from "@sog-loader/core";
 
 export const createSphere = (size: number, position: Vector3) => {
   const sphere = MeshBuilder.CreateSphere("sphere", { diameter: size });
@@ -12,45 +17,12 @@ export const createSphere = (size: number, position: Vector3) => {
   return sphere;
 };
 
-function _convertSplatToSPlatBinary(splat: Splat | RawSplat): ArrayBuffer {
-  const splatCount = splat.count;
-  const unitByteLength = 4 * 3 + 4 * 3 + 4 + 4; // = 32
-  const data = new ArrayBuffer(splatCount * unitByteLength);
-  const SH_C0 = 0.28209479177387814; // SH_C0 = Y_0^0 = 1 / (2 * sqrt(pi))
-
-  for (let i = 0; i < splatCount; i++) {
-    const posBuffer = new Float32Array(data, i * unitByteLength, 3);
-    posBuffer[0] = splat.position[i * 3 + 0];
-    posBuffer[1] = splat.position[i * 3 + 1];
-    posBuffer[2] = splat.position[i * 3 + 2];
-
-    const scaleBuffer = new Float32Array(data, i * unitByteLength + 12, 3);
-    scaleBuffer[0] = Math.exp(splat.scale[i * 3 + 0]);
-    scaleBuffer[1] = Math.exp(splat.scale[i * 3 + 1]);
-    scaleBuffer[2] = Math.exp(splat.scale[i * 3 + 2]);
-
-    const colorBuffer = new Uint8ClampedArray(data, i * unitByteLength + 24, 4);
-    colorBuffer[0] = (SH_C0 * splat.sh0[i * 4 + 0] + 0.5) * 255;
-    colorBuffer[1] = (SH_C0 * splat.sh0[i * 4 + 1] + 0.5) * 255;
-    colorBuffer[2] = (SH_C0 * splat.sh0[i * 4 + 2] + 0.5) * 255;
-    colorBuffer[3] = (1 / (1 + Math.exp(-splat.sh0[i * 4 + 3]))) * 255;
-
-    const rotBuffer = new Uint8ClampedArray(data, i * unitByteLength + 28, 4);
-    rotBuffer[0] = splat.rotation[i * 4 + 0] * 127.5 + 127.5;
-    rotBuffer[1] = splat.rotation[i * 4 + 1] * 127.5 + 127.5;
-    rotBuffer[2] = splat.rotation[i * 4 + 2] * 127.5 + 127.5;
-    rotBuffer[3] = splat.rotation[i * 4 + 3] * 127.5 + 127.5;
-  }
-
-  return data;
-}
-
 export async function createGsFromSogFile(
   sogFile: ArrayBuffer,
   scene: Scene,
 ): Promise<GaussianSplattingMesh> {
   const sogData = unpackRaw(new Uint8Array(sogFile));
-  const splat = decode(sogData);
+  using splat = decodeRaw(sogData);
   const binarySplat = _convertSplatToSPlatBinary(splat);
   const sh = _createShTextureBuffers(splat, scene);
   const gsMesh = new GaussianSplattingMesh("splat", undefined, scene, true);
@@ -59,14 +31,55 @@ export async function createGsFromSogFile(
   return gsMesh;
 }
 
+function _convertSplatToSPlatBinary(splat: Splat | RawSplat): ArrayBuffer {
+  const splatCount = splat.count;
+  const unitByteLength = 4 * 3 + 4 * 3 + 4 + 4; // = 32
+  const data = new ArrayBuffer(splatCount * unitByteLength);
+  const SH_C0 = 0.28209479177387814; // SH_C0 = Y_0^0 = 1 / (2 * sqrt(pi))
+
+  const position = splat.position;
+  const scale = splat.scale;
+  const rotation = splat.rotation;
+  const sh0 = splat.sh0;
+
+  for (let i = 0; i < splatCount; i++) {
+    const posBuffer = new Float32Array(data, i * unitByteLength, 3);
+    posBuffer[0] = position[i * 3 + 0];
+    posBuffer[1] = position[i * 3 + 1];
+    posBuffer[2] = position[i * 3 + 2];
+
+    const scaleBuffer = new Float32Array(data, i * unitByteLength + 12, 3);
+    scaleBuffer[0] = Math.exp(scale[i * 3 + 0]);
+    scaleBuffer[1] = Math.exp(scale[i * 3 + 1]);
+    scaleBuffer[2] = Math.exp(scale[i * 3 + 2]);
+
+    const colorBuffer = new Uint8ClampedArray(data, i * unitByteLength + 24, 4);
+    colorBuffer[0] = (SH_C0 * sh0[i * 4 + 0] + 0.5) * 255;
+    colorBuffer[1] = (SH_C0 * sh0[i * 4 + 1] + 0.5) * 255;
+    colorBuffer[2] = (SH_C0 * sh0[i * 4 + 2] + 0.5) * 255;
+    colorBuffer[3] = (1 / (1 + Math.exp(-sh0[i * 4 + 3]))) * 255;
+
+    const rotBuffer = new Uint8ClampedArray(data, i * unitByteLength + 28, 4);
+    rotBuffer[0] = rotation[i * 4 + 0] * 127.5 + 127.5;
+    rotBuffer[1] = rotation[i * 4 + 1] * 127.5 + 127.5;
+    rotBuffer[2] = rotation[i * 4 + 2] * 127.5 + 127.5;
+    rotBuffer[3] = rotation[i * 4 + 3] * 127.5 + 127.5;
+  }
+
+  return data;
+}
+
 function _createShTextureBuffers(
-  {
-    count: splatCount,
-    shN,
-    sh_degree,
-  }: { shN: Float32Array; count: number; sh_degree: number } | Splat | RawSplat,
+  splat:
+    | { shN: Float32Array; count: number; sh_degree: number }
+    | Splat
+    | RawSplat,
   scene: Scene,
 ): Array<Uint8Array> | null {
+  const shN = splat.shN;
+  const sh_degree = splat.sh_degree;
+  const splatCount = splat.count;
+
   if (!shN) {
     return null;
   }
